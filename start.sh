0400dc4c7a9b07560a9592ab2b3dca0bc3c2fe28
Mdiskbbot <158266051+Mdiskbbot@users.noreply.github.com>
2024-02-01 11:21:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /advanced_autofilter_mbbot
 fi
-cd /VJ-FILTER-BOT 
+cd /advanced_autofilter_mbbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
